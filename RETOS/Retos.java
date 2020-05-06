1d1f620abe0774d794f85eea6fe4d37f913dbfe8
MaikyTerry <U18213654@utp.edu.pe>
2020-05-06 15:06:06 -0500

    reto

--- RETOS/Retos.java
+++ RETOS/Retos.java
@@ -1,16 +1,36 @@
 package retos;
 import java.util.Scanner;
 public class Retos {
-	static double num1, num2;
+	static double num1, num2, num3;
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		System.out.println("Ingrese dos números");
-		num1 = scan.nextDouble();
-		num2 = scan.nextDouble();
-		if(num1<=num2) {
-			System.out.println(num2+ "," + num1);
-		}else {
-			System.out.println(num1+ "," + num2);
+		System.out.println("Ingrese tres números");
+		num1 = scan.nextInt();
+		num2 = scan.nextInt();
+		num3 = scan.nextInt();
+		if(num1>num2 && num2>num3) {
+			System.out.println(num1+" y "+num2+" y "+num3);
+		}else{
+			if(num1>num3 && num3>num2) {
+			System.out.println(num1+" y "+num3+" y "+num2);
+		}else{
+			System.out.println(num3+" y "+num2+" y "+num1);
+			if(num2>num1 && num1>num3) {
+				System.out.println(num2+" y "+num1+" y "+num3);
+			}else {
+				if(num2>num3 && num3>num1) {
+					System.out.println(num2+" y "+num3+" y "+num1);
+				}else {
+					if(num3>num1 && num1>num2) {
+						System.out.println(num3+" y "+num1+" y "+num2);
+					}else {
+						if(num3>num2 && num2>num1) {
+							System.out.println(num3+" y "+num2+" y "+num1);
+						}
+					}
+				}
+			}
+			}
 		}
 	}
-}
+}	
