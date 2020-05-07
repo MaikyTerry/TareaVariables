884457ed8d67938508a19898098dc6ebd52765d0
MaikyTerry <U18213654@utp.edu.pe>
2020-05-07 14:43:20 -0500

    retos

--- RETOS/Retos.java
+++ RETOS/Retos.java
@@ -1,15 +1,19 @@
 package retos;
+
 import java.util.Scanner;
+
 public class Retos {
-	static int num;	
+	static int num1, num2;
+
 	public static void main(String[] args) {
-		Scanner scan = new Scanner (System.in);	
-		System.out.println("Ingrese un número");
-		num = scan.nextInt();
-		if (num < 0) {
-			System.out.println("Su numero es negativo");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ingrese dos números");
+		num1 = scan.nextInt();
+		num2 = scan.nextInt();
+		if ( num1 % num2 == 0) {
+			System.out.println("Son multiplos");
 		}else {
-			System.out.println("Su numero es positivo");
+			System.out.println("no son multiplos");
 		}
-	}	
-}	
+	}
+}
