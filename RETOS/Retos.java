8e4e2972c3cbb7adcdf5da4bac30953f6d50e8e9
MaikyTerry <U18213654@utp.edu.pe>
2020-05-07 14:55:51 -0500

    retos

--- RETOS/Retos.java
+++ RETOS/Retos.java
@@ -3,17 +3,26 @@ package retos;
 import java.util.Scanner;
 
 public class Retos {
-	static int num1, num2;
+	static int num1;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Ingrese dos números");
+		System.out.println("Ingrese un numero entre 0 y 9999");
 		num1 = scan.nextInt();
-		num2 = scan.nextInt();
-		if ( num1 % num2 == 0) {
-			System.out.println("Son multiplos");
+		if (num1<10){
+			System.out.println("El numero tiene una cifra");
 		}else {
-			System.out.println("no son multiplos");
+			if (num1<100) {
+				System.out.println("El numero tiene dos cifra");
+			}else {
+				if (num1<1000) {
+					System.out.println("El numero tiene tres cifra");
+				}else {
+					if (num1<10000) {
+						System.out.println("El numero tiene cuatro cifras");
+					}
+				}
+			}
 		}
 	}
 }
