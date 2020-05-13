ea2666103a52dfbf6d8f7d43c5fd17a237693d6f
MaikyTerry <U18213654@utp.edu.pe>
2020-05-13 15:19:01 -0500

    sueldos das

--- RETOS/UTP.java
+++ RETOS/UTP.java
@@ -5,17 +5,31 @@ import java.util.Scanner;
 public class UTP {
 
 	public static void main(String[] args) {
-		int sueldo;
-		double ns, sf;
+		int cate;
+		double sue, tsue;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Ingrese su sueldo");
-		sueldo = scan.nextInt();
-		if (sueldo<1000) {
-			ns=sueldo*0.15;
-			sf=sueldo +ns;
-			System.out.println("Su sueldo nuevo sueldo sera: "+sf);
-		}else
-			System.out.println("su sueldo es el mismo: "+sueldo);
-		
+		System.out.println("Cual es su categortia y su sueldo");
+		cate = scan.nextInt();
+		sue =scan.nextDouble();
+		switch (cate) {
+			case 1:
+				tsue = sue*1.15;
+				System.out.println("su sueldo total sera: "+tsue);
+				break;
+			case 2:
+				tsue = sue*1.10;
+				System.out.println("su sueldo total sera: "+tsue);
+				break;
+			case 3:
+				tsue = sue * 1.08;
+				System.out.println("su sueldo total sera: "+tsue);
+				break;
+			case 4:
+				tsue = sue * 1.07;
+				System.out.println("su sueldo total sera: "+tsue);
+				break;
+			default :
+				System.out.println("ERROR!");				
+		}
 	}
 }
